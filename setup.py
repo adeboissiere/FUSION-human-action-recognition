fc1451b31dcd648f7bb9b57a2cf8913d18e6a1f7
Alban <alban.deboissiere@gmail.com>
2019-06-12 15:35:48 -0400

    first commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='State of the art algorithms on the NTU-RGB-D public dataset',
+    author='Alban Main de Boissiere',
+    license='MIT',
+)
