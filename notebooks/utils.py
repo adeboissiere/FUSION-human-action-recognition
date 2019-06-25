2d214d85e2925b66415e7448c520a5fa78011794
Alban <alban.deboissiere@gmail.com>
2019-06-25 15:48:29 -0400

    Worked on JN. Creating h5 dataset.

--- notebooks/utils.py
+++ notebooks/utils.py
@@ -1,4 +1,51 @@
+# Hyper parameters
+crop_size = 50
+
 import numpy as np
+from joints import *
+
+
+def extract_hands(skeleton_rgb, videodata, crop_size):
+    # videodata shape (max_frame, 1080, 1920, 3)
+    # skeleton_rgb shape (2{yx}, max_frame, num_joint=25, 2)
+    hand_crops = []
+    
+    max_x = videodata.shape[1]
+    max_y = videodata.shape[2]
+    
+    offset = int(crop_size / 2)
+    
+    n_frames = skeleton_rgb.shape[1]
+    n_subjects = 1
+    
+    # Check if some coordinates for skeleton 2 are != 0
+    if np.any(skeleton_rgb[:, :, :, 1]):
+        n_subjects = 2
+    
+    # Get correspoding time coordinates
+    for s in range(n_subjects):
+        hand_crops_s = np.zeros((n_frames, 2, crop_size, crop_size, 3), dtype=np.uint8)
+        
+        for t in range(n_frames):
+            # Get right/left hand center coordinates
+            left_hand_x = min(int(skeleton_rgb[1, t, Joints.HANDLEFT, s]), max_x)
+            left_hand_y = min(int(skeleton_rgb[0, t, Joints.HANDLEFT, s]), max_y)
+            
+            right_hand_x = int(skeleton_rgb[1, t, Joints.HANDRIGHT, s])
+            right_hand_y = int(skeleton_rgb[0, t, Joints.HANDRIGHT, s])
+            
+            frame = np.pad(videodata[t], ((offset, offset), (offset, offset), (0, 0)), mode='constant') # shape(1130, 1970, 3)
+            
+            hand_crops_s[t, 0] = frame[left_hand_x:left_hand_x+2*offset, left_hand_y:left_hand_y+2*offset]
+            hand_crops_s[t, 1] = frame[right_hand_x:right_hand_x+2*offset, right_hand_y:right_hand_y+2*offset]
+            
+        hand_crops.append(hand_crops_s)
+    
+    return np.concatenate(hand_crops, axis = 1)
+
+
+
+
 
 # Code courtesy of yysijie and the awesome paper ST-GCN
 # https://github.com/yysijie/st-gcn/
