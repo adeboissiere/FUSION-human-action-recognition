847d910f774e0ae3653ddc9ad3d712f9d151d21b
Alban <alban.deboissiere@gmail.com>
2019-08-30 12:15:19 -0400

    VA-CNN kinematic

--- run_compute_canada.sh
+++ run_compute_canada.sh
@@ -2,7 +2,7 @@
 #SBATCH --gres=gpu:1       # Request GPU "generic resources"
 #SBATCH --cpus-per-task=6  # Cores proportional to GPUs: 6 on Cedar, 16 on Graham.
 #SBATCH --mem=32000M       # Memory proportional to GPUs: 32000 Cedar, 64000 Graham.
-#SBATCH --time=0-36:00
+#SBATCH --time=0-12:00
 #SBATCH --output=%N-%j.out
 
 module load python/3.7
@@ -16,12 +16,12 @@ make train NTU_RGBD_DATA_PATH="../../../datasets/" EVALUATION_TYPE=cross_subject
 			LEARNING_RATE=1e-4 \
 			WEIGHT_DECAY=0.0 \
 			GRADIENT_THRESHOLD=1 \
-			EPOCHS=50 \
+			EPOCHS=25 \
 			BATCH_SIZE=32 \
 			SUB_SEQUENCE_LENGTH=20 \
 			NORMALIZE_SKELETON=True \
 			NORMALIZATION_TYPE=1-COORD-SYS \
 			KINEMATIC_CHAIN_SKELETON=True \
 			AUGMENT_DATA=True \
-			USE_VALIDATION=True \
+			USE_VALIDATION=False \
 			EVALUATE_TEST=True
