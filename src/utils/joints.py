9bdfe68d317a7cd2a54346dd512cf3639cef856d
Alban <alban.deboissiere@gmail.com>
2019-09-02 18:30:56 -0400

    Created notebook showcasing skeleton scaling strategy and algorithms

--- src/utils/joints.py
+++ src/utils/joints.py
@@ -68,26 +68,26 @@ connexion_tuples = np.array([[Joints.SPINEBASE, Joints.SPINEMID],
                              [Joints.SPINESHOULDER, Joints.NECK],
                              [Joints.NECK, Joints.HEAD],
 
-                             [Joints.SPINESHOULDER, Joints.SHOULDERLEFT],
+                             [Joints.SPINESHOULDER, Joints.SHOULDERLEFT], # 4
                              [Joints.SHOULDERLEFT, Joints.ELBOWLEFT],
                              [Joints.ELBOWLEFT, Joints.WRISTLEFT],
                              [Joints.WRISTLEFT, Joints.HANDLEFT],
                              [Joints.HANDLEFT, Joints.HANDTIPLEFT],
                              [Joints.HANDLEFT, Joints.THUMBLEFT],
 
-                             [Joints.SPINESHOULDER, Joints.SHOULDERRIGHT],
+                             [Joints.SPINESHOULDER, Joints.SHOULDERRIGHT], # 10
                              [Joints.SHOULDERRIGHT, Joints.ELBOWRIGHT],
                              [Joints.ELBOWRIGHT, Joints.WRISTRIGHT],
                              [Joints.WRISTRIGHT, Joints.HANDRIGHT],
                              [Joints.HANDRIGHT, Joints.HANDTIPRIGHT],
                              [Joints.HANDRIGHT, Joints.THUMBRIGHT],
 
-                             [Joints.SPINEBASE, Joints.HIPRIGHT],
+                             [Joints.SPINEBASE, Joints.HIPRIGHT], # 16
                              [Joints.HIPRIGHT, Joints.KNEERIGHT],
                              [Joints.KNEERIGHT, Joints.ANKLERIGHT],
                              [Joints.ANKLERIGHT, Joints.FOOTRIGHT],
 
-                             [Joints.SPINEBASE, Joints.HIPLEFT],
+                             [Joints.SPINEBASE, Joints.HIPLEFT], # 20
                              [Joints.HIPLEFT, Joints.KNEELEFT],
                              [Joints.KNEELEFT, Joints.ANKLELEFT],
                              [Joints.ANKLELEFT, Joints.FOOTLEFT]])  # shape (n_connexions, 2)
