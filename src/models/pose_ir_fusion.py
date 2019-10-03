Fixed log(softmax) with small epsilon

src/models/pose_ir_fusion.py
@@ -61,7 +61,7 @@ class Fusion(nn.Module):
         pred = (weighted_prediction[:, 0] * pred_pose.transpose(1, 0)
                 + weighted_prediction[:, 1] * pred_ir.transpose(1, 0)).transpose(1, 0)
         '''
-        return torch.log(pred)
+        return torch.log(pred + 1e-12)
 
 
 def prime_X_fusion(X):

src/models/train_utils.py
@@ -107,9 +107,9 @@ def evaluate_set(model, model_type, data_loader, output_folder, set_name):
         y_true.append(Y)
         y_pred.append(Y_hat)
 
-        print([classes[i] for i in np.int32(Y_hat)])
-        print([classes[i] for i in np.int32(Y)])
-        print()
+        # print([classes[i] for i in np.int32(Y_hat)])
+        # print([classes[i] for i in np.int32(Y)])
+        # print()
 
         batch_log = open(output_folder + "batch_log.txt", "a+")
         batch_log.write("[" + str(set_name) + " - " + str(batch_idx) + "/" + str(len(data_loader)) +