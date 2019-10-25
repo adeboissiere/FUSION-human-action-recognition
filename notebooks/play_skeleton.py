92b5cbd9757874e2821ea4b30b758e1c1d4e6f86
Alban <alban.deboissiere@gmail.com>
2019-10-25 10:25:48 -0400

    Lala

--- notebooks/play_skeleton.py
+++ notebooks/play_skeleton.py
@@ -23,21 +23,53 @@ def animateJointCoordinates(joint_coordinates, connexion_tuples):
 
     
     for t in range(joint_coordinates.shape[2]):
+    # for t in range(1):
         ax.clear()
-        ax.set_xlim3d(np.amin(joint_coordinates[:, x, :]), np.amax(joint_coordinates[:, x, :]))
-        ax.set_ylim3d(np.amin(joint_coordinates[:, y, :]), np.amax(joint_coordinates[:, y, :]))
-        ax.set_zlim3d(np.amin(joint_coordinates[:, z, :]), np.amax(joint_coordinates[:, z, :]))
         
-        ax.scatter(joint_coordinates[:, x, t], joint_coordinates[:, y, t], joint_coordinates[:, z, t])
+        # Camera coordinate system
+        axis_length = 0.2
+        ax.scatter([0], [0], [0], color="red")
+        ax.scatter([axis_length, 0, 0], [0, axis_length, 0], [0, 0, axis_length], marker="v", color="red")
         
+        x_axis = plt3d.art3d.Line3D([0, axis_length], [0, 0], [0, 0])
+        x_axis.set_color("red")
+        y_axis = plt3d.art3d.Line3D([0, 0], [0, axis_length], [0, 0])
+        y_axis.set_color("red")
+        z_axis = plt3d.art3d.Line3D([0, 0], [0, 0], [0, axis_length])
+        z_axis.set_color("red")
+        ax.add_line(x_axis)
+        ax.add_line(y_axis)
+        ax.add_line(z_axis)
         
-        line = plt3d.art3d.Line3D([0.1, 0.1], [0.1, 0.5], [0.1, 0.1])
+        # New coordinate system
+        x_spine_mid = joint_coordinates[1, x, 0]
+        y_spine_mid = joint_coordinates[1, y, 0]
+        z_spine_mid = joint_coordinates[1, z, 0]
+        ax.scatter(x_spine_mid, y_spine_mid, z_spine_mid, color="green")
+        ax.scatter([x_spine_mid + axis_length, x_spine_mid, x_spine_mid], 
+                   [y_spine_mid, y_spine_mid + axis_length, y_spine_mid], 
+                   [z_spine_mid, z_spine_mid, z_spine_mid + axis_length], marker="v", color="green")
         
-        head_neck = plt3d.art3d.Line3D([joint_coordinates[0, x, t], joint_coordinates[1, x, t]], 
-                                      [joint_coordinates[0, y, t], joint_coordinates[1, y, t]], 
-                                      [joint_coordinates[0, z, t], joint_coordinates[1, z, t]])
+        x_axis = plt3d.art3d.Line3D([x_spine_mid, x_spine_mid + axis_length], [y_spine_mid, y_spine_mid], [z_spine_mid, z_spine_mid])
+        x_axis.set_color("green")
+        y_axis = plt3d.art3d.Line3D([x_spine_mid, x_spine_mid], [y_spine_mid, y_spine_mid + axis_length], [z_spine_mid, z_spine_mid])
+        y_axis.set_color("green")
+        z_axis = plt3d.art3d.Line3D([x_spine_mid, x_spine_mid], [y_spine_mid, y_spine_mid], [z_spine_mid, z_spine_mid + axis_length])
+        z_axis.set_color("green")
+        ax.add_line(x_axis)
+        ax.add_line(y_axis)
+        ax.add_line(z_axis)
         
-        # ax.add_line(head_neck)
+        # Translation vector
+        trans_vec = plt3d.art3d.Line3D([0, x_spine_mid], [0, y_spine_mid], [0, z_spine_mid], linestyle='--', color="black")
+        ax.add_line(trans_vec)
+        
+        # Subject coordinates
+        ax.set_xlim3d(min(np.amin(joint_coordinates[:, x, :]),-axis_length), max(np.amax(joint_coordinates[:, x, :]), axis_length))
+        ax.set_ylim3d(min(np.amin(joint_coordinates[:, y, :]),-axis_length), max(np.amax(joint_coordinates[:, y, :]), axis_length))
+        ax.set_zlim3d(min(np.amin(joint_coordinates[:, z, :]),-axis_length), max(np.amax(joint_coordinates[:, z, :]), axis_length))
+        
+        ax.scatter(joint_coordinates[:, x, t], joint_coordinates[:, y, t], joint_coordinates[:, z, t], color="blue")
         
         for i in range(connexion_tuples.shape[0]):
             j1 = connexion_tuples[i, 0]
@@ -46,7 +78,7 @@ def animateJointCoordinates(joint_coordinates, connexion_tuples):
             
             joint_line = plt3d.art3d.Line3D([joint_coordinates[j1, x, t], joint_coordinates[j2, x, t]], 
                                             [joint_coordinates[j1, y, t], joint_coordinates[j2, y, t]], 
-                                            [joint_coordinates[j1, z, t], joint_coordinates[j2, z, t]])
+                                            [joint_coordinates[j1, z, t], joint_coordinates[j2, z, t]], linestyle=':')
             
             ax.add_line(joint_line)
         
